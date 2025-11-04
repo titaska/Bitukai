04a141e5c74ad56f0b8ffccf7d68a6d30bf3298f
tika0030 <titas.karitonas@mif.stud.vu.lt>
2025-11-04 22:48:14 +0200

    Initial commit: .NET 8 starter API

---
+++ src/Pos.Api/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/health", () => Results.Ok(new { status = "ok", service = "Pos.Api" }))
+   .WithName("Health")
+   .WithTags("System");
+
+app.Run();
